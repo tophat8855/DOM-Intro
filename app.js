ab01674d6c6f4e979aec6362723e7449d19bc5ac
tophat8855 <hrfarley@gmail.com>
2015-01-14 12:04:09 -0800

    render li elements in yellow using js

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log("JavaScript is alive");
 
 var greeting_div = document.getElementById("greeting");
 greeting_div.innerHTML = "Hi Planet Earth";
+
+function yellowList(){
+  var lis = document.querySelectorAll('li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].style.backgroundColor = "yellow";
+  }
+}
+yellowList()
